f97daddd3bff39ff27ae98620509165dfa5f5c5b
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-10-07 09:04:03 +0200

    // Version updated to v1.3.2

--- blockcontact.php
+++ blockcontact.php
@@ -34,7 +34,7 @@ class Blockcontact extends Module
 		$this->name = 'blockcontact';
 		$this->author = 'PrestaShop';
 		$this->tab = 'front_office_features';
-		$this->version = '1.3.1';
+		$this->version = '1.3.2';
 
 		$this->bootstrap = true;
 		parent::__construct();	
